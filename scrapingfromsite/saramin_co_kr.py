21543c1f26f340e2893d854e1391d4feaceb2aad
SanghunOh <otter.oh@gmail.com>
2021-03-24 16:44:38 +0900

    modify saramin.co.kr

--- scrapingfromsite/saramin_co_kr.py
+++ scrapingfromsite/saramin_co_kr.py
@@ -7,32 +7,45 @@ res = requests.get(url_main+url_param)
 
 # res.status_code, res.content
 soup = BeautifulSoup(res.content, 'lxml')
-groups = soup.find_all(name='li', attrs={'class': ['item','lookup', 'effect_bold']})
+groups = soup.find_all(name=['li', 'div'], attrs={'class': ['item','lookup', 'effect_bold', 'list_item']})
 
+div.list_row>.col
 surfix_url = 'https://www.saramin.co.kr'
-
 total_count = 0
 print('total groups : ', len(groups))
 for group in groups:
     divide = group['class'][0].strip()
-    if divide == 'item':
+    if divide == 'item':        # 그랜드 상품
         detail_url = surfix_url + group.a['href']       # 상세 링크
         company_name = group.a.contents[3].string       # 회사명
         recruit_title = group.strong.string             # 모집 주제
         apply_end_date = group.contents[5].string       # 마감일
         need_career = group.ul.contents[1].string       # 경력
         need_education = group.ul.contents[3].string    # 학력
-        work_local = group.ul.contents[5].string        # 근무지역
+        employment_type = ''    # 채용 종류
+        work_place = group.ul.contents[5].string        # 근무지역
         total_count += 1
 
-    elif divide in ['lookup', 'effect_bold'] :
+    elif divide in ['lookup', 'effect_bold'] :      # 프리미엄, 포커스 상품
         detail_url = surfix_url + group.a['href']       # 상세 링크
         company_name = group.h3.string                  # 회사명
         recruit_title = group.h4.string                 # 모집 주제
         apply_end_date = group.contents[5].span.string  # 마감일
         need_career = group.ul.li.string       # 경력
         need_education = group.ul.contents[3].string    # 학력
-        work_local = group.ul.contents[5].string        # 근무지역
+        employment_type = ''    # 채용 종류
+        work_place = group.ul.contents[5].string        # 근무지역
+        total_count += 1
+
+    elif divide == 'list_item' :        # 전체 채용 정보
+        detail_url = surfix_url + group.a['href']       # 상세 링크
+        company_name = group.contents[3].span.string    # 회사명
+        recruit_title = group.contents[3].span.string                 # 모집 주제
+        apply_end_date = group.contents[11].contents[3].contents[0]  # 마감일
+        need_career = group.contents[7].p.string       # 경력
+        need_education = group.contents[7].contents[1].string    # 학력
+        employment_type = group.contents[9].p.string    # 채용 종류
+        work_place = group.contents[9].contents[1].string        # 근무지역
         total_count += 1
 
     # compare to DB
